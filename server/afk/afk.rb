79470c9331c70d5d2926f15f18f6de658e06d3f3
Ondřej Čapek <7-7948-1@plus4u.net>
2024-08-17 14:53:33 +0200

    schedule 2024

--- server/afk/afk.rb
+++ server/afk/afk.rb
@@ -11,20 +11,19 @@ require "nokogiri"
 
 TEAMS = {
   "TJ AFK Bratčice" => 1,
-  "FC Bílé Podolí B" => 5,
   "FK Chotusice 1932 B" => 47,
   "TJ Dynamo Horní Bučice" => 2,
   "Sokol Močovice" => 50,
   "SK Nepoměřice" => 56,
-  "TJ Rataje nad Sázavou" => 11,
   "FK KAVALIER SÁZAVA B" => 13,
-  "TJ Sokol Družba Suchdol B" => 14,
   "TJ Star Tupadly B" => 12,
   "FK Uhlířské Janovice B" => 62,
   "TJ Sokol Vlkaneč" => 46,
-  "TJ Slavoj Vrdy B" => 48,
   "SK Zbraslavice" => 60,
-  "TJ Sokol Žlebské Chvalovice." => 33,
+  "TJ Sokol Malín" => 63,
+  "SK Malešov B" => 64,
+  "SK  Spartak Žleby" => 35,
+  "FK Záboří nad Labem" => 38
 }
 
 doc = Nokogiri::HTML5(File.open("schedule.html"))
@@ -38,7 +37,7 @@ rounds.each do |round_el|
   round_matches = round_el.xpath("./ul/li[contains(@class, 'js-matchRound')]")
 
   round_matches.each do |round_match|
-    day = round_match.xpath(".//ul[contains(@class, 'MatchRound-meta')]/li[1]/p")[0].text
+    day = round_match.xpath(".//ul[contains(@class, 'MatchRound-meta')]/li[1]/p")[0].text.gsub(/\n/, "").gsub(/\u00A0/, " ")
     date_match = day.match(/(\d+)\.\s*(\d+)\.\s*(\d+)\s+(\d+):(\d+)/)
     date = "#{date_match[3]}-#{date_match[2].rjust(2, "0")}-#{date_match[1].rjust(2, "0")} #{date_match[4]}:#{date_match[5]}:00"
 
